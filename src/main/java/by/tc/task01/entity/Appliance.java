804e031b9df0b8bdab4471498b9f62b3dfdcff37
lwi-wil <nikstrach@gmail.com>
2021-11-02 16:09:26 +0300

    gitignore

---
+++ src/main/java/by/tc/task01/entity/Appliance.java
@@ -0,0 +1,22 @@
+package by.tc.task01.entity;
+
+public class Appliance {
+    private float cost;
+    private String name;
+	public void setCost(float cost){
+	    this.cost = cost;
+    }
+
+    public float getCost(){
+	    return cost;
+    }
+
+    public void setName(String name){
+	    this.name = name;
+    }
+
+    public String getName(){
+	    return name;
+    }
+
+}
